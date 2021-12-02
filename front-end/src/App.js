26cc512c0c8045dc08601f6ab5b7e53ca14e9f65
tangjm <jaredtang@hotmail.co.uk>
2021-12-02 16:04:24 +0000

    Added axios, dotenv; configured axios post request to send relevant data

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,45 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css';
 
 function App() {
+  const [playerName, setPlayerName] = useState(``);
+
+  const [displayResult, setDisplayResult] = useState(false);
+
   const nameField = "Please input your name";
   const optionField = "Rock, Paper or Scissors?";
-  const availableOptions = ["Rock", "Paper", "Scissors"];
+  const possibleOptions = ["rock", "paper", "scissors"];
 
-  const port = 
+  const nodeServer = process.env.REACT_APP_NODE_SERVER;
+
   const submitHandler = event => {
     event.preventDefault();
-    axios.post(``)
+    const chosenOption = document.querySelector("input[name='options']:checked").value;
+    submitData(playerName, chosenOption);
+    setPlayerName(``);
+  }
 
+  const submitData = async (playerName, chosenOption) => {
+    try {
+      const res = await axios.post(
+        `${nodeServer}/finalised`,
+        { name: playerName, choice: chosenOption }
+      );
+      // return res.data;
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <div className="App-header">
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <h3>{nameField}</h3>
-      <input className="App-link" required />
-      <h3>{optionField}</h3>
-
-      <form onClick={submitHandler}>
+      <form onSubmit={submitHandler}>
         <div>
-          <input type="radio" id={availableOptions[0]} name="options" value={availableOptions[0]} required />
-          <label for={availableOptions[0]}>{availableOptions[0]}</label>
+          <h3>{nameField}</h3>
+          <label htmlFor="name">{nameField}</label>
+          <input
+            className="App-link"
+            id="name"
+            value={playerName}
+            placeholder="Name"
+            onChange={event => setPlayerName(event.target.value)}
+            required
+          />
         </div>
+        <h3>{optionField}</h3>
         <div>
-          <input type="radio" id={availableOptions[1]} name="options" value={availableOptions[1]} required />
-          <label for={availableOptions[1]}>{availableOptions[1]}</label>
-        </div>
-        <div>
-          <input type="radio" id={availableOptions[2]} name="options" value={availableOptions[2]} required />
-          <label for={availableOptions[2]}>{availableOptions[2]}</label>
+          <input
+            type="radio"
+            id={possibleOptions[0]}
+            name="options"
+            value={possibleOptions[0]}
+            required
+          />
+          <label htmlFor={possibleOptions[0]}>{possibleOptions[0]}</label>
+
+          <input
+            type="radio"
+            id={possibleOptions[1]}
+            name="options"
+            value={possibleOptions[1]}
+            required
+          />
+          <label htmlFor={possibleOptions[1]}>{possibleOptions[1]}</label>
+
+          <input
+            type="radio"
+            id={possibleOptions[2]}
+            name="options"
+            value={possibleOptions[2]}
+            required
+          />
+          <label htmlFor={possibleOptions[2]}>{possibleOptions[2]}</label>
         </div>
 
-        <input type="submit" className="App-link" value="submit" />
-
+        <input type="submit" className="App-link" value="Submit" />
       </form>
     </div >
   );
