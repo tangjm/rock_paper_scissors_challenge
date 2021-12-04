2802d5c76216c2d2fa5dea679c03215d389c5c72
tangjm <jaredtang@hotmail.co.uk>
2021-12-04 16:33:35 +0000

    Updated UI

--- front-end/src/Components/ResultsPage.jsx
+++ front-end/src/Components/ResultsPage.jsx
@@ -3,39 +3,25 @@ import PropTypes from 'prop-types';
 const ResultsPage = ({ gameResult, setGameResult, setRenderGameFields, setPlayerTurn, setPlayer1Name, setPlayer2Name }) => {
 	const pageContent = () => {
 		if (gameResult === "draw") {
-			return <h2>Not bad, a draw!</h2>
+			return <h1>Not bad, a draw!</h1>
 		}
-		return <h2>Winner: {gameResult}</h2>
+		return <h1>Winner: {gameResult}</h1>
 	}
 
 	const gameFinishedHandler = event => {
-		// The state of our states when the ResultsPage is rendered
-		// playerTurn = 1;
-		// player1Name = "name of player1";
-		// player2Name = "name of player2";
-		// gameResult = { result: "string" };
-		// renderGameFields = true;
-
 		if (event.target.name === "newPlayers") {
-			// If there are new players
 			setRenderGameFields(false);
-			// Render WelcomePage
-			// Reset player names
 			setPlayer1Name(``);
 			setPlayer2Name(``);
 		}
-		// Playing again with same players
 		setGameResult({});
-		// Render GameFields for player1 by setting playerTurn to 0
 		setPlayerTurn(0);
 	}
 
 	return (
 		<>
 			{pageContent()}
-
-			<input type="submit" name="playAgain" value="Play Again" onClick={gameFinishedHandler} />
-
+			<input variant="primary" type="submit" name="playAgain" value="Play Again" onClick={gameFinishedHandler} />
 			<input type="submit" name="newPlayers" value="New Players?" onClick={gameFinishedHandler} />
 		</>
 	)
--- front-end/src/Components/WelcomePage.jsx
+++ front-end/src/Components/WelcomePage.jsx
@@ -2,34 +2,38 @@ import PropTypes from 'prop-types';
 
 const WelcomePage = ({ player1Name, player2Name, setPlayer1Name, setPlayer2Name, submitPlayerNames }) => {
 
-	const nameField = "Please input your name";
+	const nameField = "Please register your names";
 	const handleSubmitName = event => {
 		event.preventDefault();
 		submitPlayerNames(player1Name, player2Name);
-		// setPlayer1Name(``);
-		// setPlayer2Name(``);
 	}
+
 	return (
-		<form onSubmit={handleSubmitName}>
-			<label htmlFor="name"><h3>{nameField}</h3></label>
-			<input
-				className="App-link"
-				id="name"
-				value={player1Name}
-				placeholder="Name"
-				onChange={event => setPlayer1Name(event.target.value)}
-				required
-			/>&nbsp;
-			<input
-				className="App-link"
-				id="name"
-				value={player2Name}
-				placeholder="Name"
-				onChange={event => setPlayer2Name(event.target.value)}
-				required
-			/>&nbsp;
-			<input type="submit" value="Submit" />
-		</form>
+		<>
+			<h1>Welcome!</h1>
+			<form onSubmit={handleSubmitName}>
+				<label htmlFor="name"><h3>{nameField}</h3></label>
+				<p>Player 1</p>
+				<input
+					className="App-link"
+					id="name"
+					value={player1Name}
+					placeholder="Player 1"
+					onChange={event => setPlayer1Name(event.target.value)}
+					required
+				/>&nbsp;
+				<p>Player 2</p>
+				<input
+					className="App-link"
+					id="name"
+					value={player2Name}
+					placeholder="Player 2"
+					onChange={event => setPlayer2Name(event.target.value)}
+					required
+				/>&nbsp;
+				<input type="submit" value="Submit" />
+			</form>
+		</>
 	)
 }
 
