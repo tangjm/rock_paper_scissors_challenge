352fa654702d630483772b28bcd0d3e8056c7230
tangjm <jaredtang@hotmail.co.uk>
2021-12-05 19:26:45 +0000

    Refactored gameSetup routes

--- back-end/routes/gameSetup.js
+++ back-end/routes/gameSetup.js
@@ -3,23 +3,12 @@ const RPS = require('../src/RPS');
 const RPSextended = require('../src/RPSextended');
 const router = express.Router();
 
-router.route(`/`)
-	.post((req, res) => {
-		if (req.body?.gameMode) {
-			return res.redirect(307, '/gameMode');
-		}
-
-		if (req.body?.gameType) {
-			return res.redirect(307, '/gameType');
-		}
-	})
-
 router.route(`/gameMode`)
 	.post((req, res) => {
 		res.status(200);
 		return (
-			req.body.gameType === "normal" ? res.json({ gameType: "normal" })
-				: res.json({ gameType: "special" })
+			req.body.gameMode === "singlePlayer" ? res.json({ gameType: "singlePlayer" })
+				: res.json({ gameType: "multiplayer" })
 		);
 	});
 
@@ -27,14 +16,27 @@ router.route(`/gameType`)
 	.post((req, res) => {
 		res.status(200);
 		if (req.body.gameType === "normal") {
-			req.app.locals.game = new RPS();
-			return res.json({ gameType: "normal" });
+			res.redirect(307, '/gameType/normal');
 		}
 
 		if (req.body.gameType === "special") {
-			req.app.locals.game = new RPSextended();
-			return res.json({ gameType: "special" });
+			res.redirect(307, '/gameType/special');
 		}
 	})
+
+router.route(`/gameType/normal`)
+	.post((req, res) => {
+		req.app.locals.game = new RPS();
+		res.status(200);
+		return res.json({ gameType: "normal" });
+	});
+
+router.route(`/gameType/special`)
+	.post((req, res) => {
+		req.app.locals.game = new RPSextended();
+		res.status(200);
+		return res.json({ gameType: "special" });
+	});
+
 
 module.exports = router;
