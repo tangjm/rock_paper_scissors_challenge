791abc171b8d0461dba8c2baaaf1822921a18368
tangjm <jaredtang@hotmail.co.uk>
2021-12-05 12:14:31 +0000

    Added routes for determining the results of single player rps games

--- back-end/routes/results.js
+++ back-end/routes/results.js
@@ -1,14 +1,43 @@
 const express = require('express');
+const Player = require('../src/Player');
 
 const router = express.Router();
+
 
 router.route(`/`)
 	.post((req, res) => {
 		let game = req.app.locals.game;
+		let playerCount = game.getPlayers().length;
+		res.status(200);
+		if (playerCount === 1) {
+			return res.redirect(307, '/singlePlayer');
+		}
+		return res.redirect(307, '/multiplayer');
+	})
+
+router.route(`/multiplayer`)
+	.post((req, res) => {
+		let game = req.app.locals.game;
+
 		let player1 = game.getPlayers()[0];
 		let player2 = game.getPlayers()[1];
 
 		const result = game.determineWinner(player1, player2);
+
+		res.status(200).json({ result });
+	})
+
+router.route(`/singlePlayer`)
+	.post((req, res) => {
+		let game = req.app.locals.game;
+
+		let user = game.getPlayer()[0];
+
+		let cpuData = game.computerChooses();
+		let cpu = new Player(cpuData.name);
+		cpu.setChoice(cpu.choice);
+
+		const result = game.determineWinner(user, cpu);
 
 		res.status(200).json({ result });
 	})
