1f1f841954a76a63dda48f2de42492d69f6ae624
tangjm <jaredtang@hotmail.co.uk>
2021-12-05 11:54:08 +0000

    Updated registration route to account for single player games

--- back-end/routes/registration.js
+++ back-end/routes/registration.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const RPS = require('../src/RPS');
 const RPSextended = require('../src/RPSextended');
 
 const router = express.Router();
@@ -6,16 +7,25 @@ const router = express.Router();
 router.route(`/`)
 	.post((req, res) => {
 		if (req.body) {
-			const { player1Name, player2Name } = req.body;
+			//if req.body contains a single key
+			// then, redirect to th
+			const singlePlayerGame = Object.keys(req.body).length === 1;
 
-			const rpcGame = new RPSextended();
-			rpcGame.registerPlayers(player1Name, player2Name);
+			if (singlePlayerGame) {
+				const { playerName } = req.body;
+				const rpsGame = new RPS();
+				rpsGame.registerPlayers(playerName);
+			} else {
+				const { player1Name, player2Name } = req.body;
+				const rpsGame = new RPSextended();
+				rpsGame.registerPlayers(player1Name, player2Name);
+			}
 
-			req.app.locals.game = rpcGame;
+			req.app.locals.game = rpsGame;
 
-			return res.status(200).json("Sucessfully registered players");
+			return res.status(200).json("Sucessfully registered player(s)");
 		}
-		return res.status(404).send("Failed to register players");
+		return res.status(404).send("Failed to register player(s)");
 	})
 
 
